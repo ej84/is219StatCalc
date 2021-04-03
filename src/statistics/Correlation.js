0c52659b796c88d2b9a5e744bb76c4ec8199dba8
ej84 <ej84@njit.edu>
2021-04-03 15:29:33 -0400

    created statistics function files.

---
+++ src/statistics/Correlation.js
@@ -0,0 +1,7 @@
+const Multiply = require('../operations/Multiply');
+const Mean = require('./Mean');
+const Add = require('../operations/Add');
+const Variance = require('./Variance');
+const Math = require('mathjs');
+const Stats = require('stats-lib');
+
